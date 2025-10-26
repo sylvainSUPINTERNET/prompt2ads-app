05d42d4120e91497bd9d236857004dfc636b57ee
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-10-26 22:26:16 +0100

    feat(motion): int

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, ElementRef, Inject, PLATFORM_ID, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { animate, scroll } from "motion"
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  @ViewChild('box') box!: ElementRef;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   protected readonly title = signal('prompt2ads-app');
+
+  ngAfterViewInit() {
+
+    if (isPlatformBrowser(this.platformId)) {
+      animate(this.box.nativeElement, 
+        { opacity: [0, 1], y: [-50, 0] },
+        { duration: 0.6 }
+      );
+    }
+
+  }
 }
