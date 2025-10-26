e0c46642535c0e7f54c40eada6f64d85299eea30
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-10-26 23:57:01 +0100

    feat(motion): int

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        loadComponent: () => import("./home/home").then(m => m.Home)
+    },
+    {
+        path: 'customer',
+        loadComponent: () => import("./customer/customer").then(m => m.Customer)
+    }
+];
