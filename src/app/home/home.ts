c0022d7bc96bf1402f98344651166e6cb83c07cd
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-10-30 00:21:04 +0100

    <ip

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,7 +2,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { Component, effect, ElementRef, Inject, PLATFORM_ID, signal, ViewChild } from '@angular/core';
 import { animate } from 'motion';
 import { Customer } from "../customer/customer";
-import { LucideAngularModule, BubblesIcon, MegaphoneIcon } from 'lucide-angular';
+import { LucideAngularModule, BubblesIcon, MegaphoneIcon, View } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
@@ -16,27 +16,38 @@ export class Home {
   readonly MegaphoneIcon = MegaphoneIcon;
 
   @ViewChild('boxTitle') boxTitle!: ElementRef;
+  @ViewChild('menuBtn') menuBtn!: ElementRef;
+  @ViewChild('mobileMenu') mobileMenu!: ElementRef;
   
   protected readonly title = signal('prompt2ads-app');
 
   user = signal<null | {id:string}>(null);
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    effect(async () => {
-      const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-      setTimeout( async () => {
-        this.user.set(await res.json());
-      }, 2000)
+    effect(() => {
+      if (isPlatformBrowser(this.platformId)) {
+        (async () => {
+          const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+          const data = await res.json();
+          setTimeout(() => this.user.set(data), 2000);
+        })();
+      }
     });
   }
 
-    ngAfterViewInit() {
-      if (isPlatformBrowser(this.platformId)) {
-        animate(
-          this.boxTitle.nativeElement, 
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Animation d’entrée du titre
+      if (this.boxTitle?.nativeElement) {
+        animate(this.boxTitle.nativeElement, 
           { opacity: [0, 1], y: [-50, 0] },
           { duration: 0.6 }
         );
       }
     }
+  }
+
+  showBurger() {
+    this.mobileMenu.nativeElement.classList.toggle("hidden");
+  }
 }
