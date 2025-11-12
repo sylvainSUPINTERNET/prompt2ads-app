2c7a11c9060c86ddcb2a82efda5b5c0b50b4127a
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-11-13 00:44:51 +0100

    x

--- src/app/project-create/project-create.ts
+++ src/app/project-create/project-create.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProjectList } from "../project-list/project-list";
 
 @Component({
   selector: 'app-project-create',
-  imports: [],
+  imports: [ProjectList],
   templateUrl: './project-create.html',
   styleUrl: './project-create.css',
 })
---
+++ src/app/project-list/project-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PlusIcon, LucideAngularModule } from 'lucide-angular';
+
+@Component({
+  selector: 'app-project-list',
+  imports: [LucideAngularModule],
+  templateUrl: './project-list.html',
+  styleUrl: './project-list.css',
+})
+export class ProjectList {
+  readonly plusIcon = PlusIcon;
+}
